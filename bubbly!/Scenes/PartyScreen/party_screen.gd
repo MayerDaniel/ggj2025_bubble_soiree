7631aa5133c27812eefb194a8b37565166aa4446
Ezequiel Martinez <Martinez.ezequiel42@gmail.com>
2025-01-24 22:47:35 -0800

    initial Godot commit

---
+++ bubbly!/Scenes/PartyScreen/party_screen.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+var example_json : Dictionary = {
+	"type": "poll",
+	"data": {
+		"question": "Fruitcake good or bad?",
+		"answers": {
+			1: "yes",
+			2: "no"
+		}
+	}
+}
+
+var example_json2 : Dictionary = {
+	"type": "response",
+	"data": {
+		"target": "<some ID for the poll, probably just an int>",
+		"answer": 1
+	}
+}
