98fb499467e7d730a1ef9609faa01ce72e9cbcf3
Dan Mayer <dan@mayer.cool>
2025-01-26 15:30:16 -0800

    qrcode generated

--- bubbly!/Scenes/PartyScreen/WebSocketListener.gd
+++ bubbly!/Scenes/PartyScreen/WebSocketListener.gd
@@ -30,6 +30,16 @@ func log_response(id, answer):
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Create an HTTP request node and connect its completion signal.
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.request_completed.connect(self._http_request_completed)
+
+	# Perform the HTTP request. The URL below returns a PNG image as of writing.
+	var error = http_request.request("http://127.0.0.1:8000/static/qrcode.png")
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
+	
 	var err = socket.connect_to_url(web_socket_url)
 	if err != OK:
 		print("Unable to connect")
@@ -41,6 +51,22 @@ func _ready() -> void:
 		# Send data.
 		#socket.send_text("Test packet")
 	pass # Replace with function body.
+
+func _http_request_completed(result, response_code, headers, body):
+	if result != HTTPRequest.RESULT_SUCCESS:
+		push_error("Image couldn't be downloaded. Try a different image.")
+
+	var image = Image.new()
+	var error = image.load_png_from_buffer(body)
+	if error != OK:
+		push_error("Couldn't load the image.")
+
+	var texture = ImageTexture.create_from_image(image)
+
+	# Display the image in a TextureRect node.
+	var texture_rect = TextureRect.new()
+	add_child(texture_rect)
+	texture_rect.texture = texture
 
 func new_activity():
 	var bubble = get_node("PartyScreen/PanelContainer/PlacementZone/Bubble&QR/MarginContainer/MarginContainer/ColorRect/BubbleBody")
