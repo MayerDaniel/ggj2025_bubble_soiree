bdc28cfacfb93b97ffb4566af879098e9b3c2e5f
Ezequiel Martinez <Martinez.ezequiel42@gmail.com>
2025-01-26 02:17:50 -0800

    StartMenu works lol

---
+++ bubbly!/Scenes/StartMenu/start_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@export var game_scene: PackedScene
+
+@onready var start_button: TextureButton = get_node("VBoxContainer/HBoxContainer/StartButton")
+@onready var quit_button: TextureButton = get_node("VBoxContainer/HBoxContainer/QuitButton")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	start_button.pressed.connect(_on_start_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_packed(game_scene)
+
+func _on_quit_button_pressed():
+	get_tree().quit()
