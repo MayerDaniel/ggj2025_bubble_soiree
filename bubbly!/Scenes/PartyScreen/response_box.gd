8f842c147f9be48e9c3585b4caf253c315ea32d0
Ezequiel Martinez <Martinez.ezequiel42@gmail.com>
2025-01-26 12:36:03 -0800

    layout big bubble

--- bubbly!/Scenes/PartyScreen/response_box.gd
+++ bubbly!/Scenes/PartyScreen/response_box.gd
@@ -7,6 +7,8 @@ extends PanelContainer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	answer_label.get_v_scroll_bar().allow_greater = true
+	answer_label.get_v_scroll_bar().allow_lesser = true
+	
 	answer_label.get_v_scroll_bar().step = 0.01
 	
 	pass # Replace with function body.
@@ -28,7 +30,7 @@ func vertical_auto_scroll_with_rescroll():
 	var current_scroll_value: float = scroll_bar.value
 	if scroll_bar.max_value > 48: ##this value is dependant on the size of the box and will need to be tweaked
 		if current_scroll_value >= scroll_bar.max_value:
-			scroll_bar.value = scroll_bar.min_value
+			scroll_bar.value = scroll_bar.min_value - 10.0
 		else:
 			current_scroll_value += 0.35
 			scroll_bar.value = current_scroll_value
