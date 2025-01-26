e9e9d156d6a4a375720f03f0af0ba4cbe65540eb
Ezequiel Martinez <Martinez.ezequiel42@gmail.com>
2025-01-26 01:59:46 -0800

    added autoscroll and rescroll to prompt boxes

--- bubbly!/Scenes/PartyScreen/prompt_and_answers.gd
+++ bubbly!/Scenes/PartyScreen/prompt_and_answers.gd
@@ -5,11 +5,17 @@ extends VBoxContainer
 @onready var answers_box : HFlowContainer = get_node("AnswersFlowBox")
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	prompt_text_label.get_v_scroll_bar().allow_greater = true
+	prompt_text_label.get_v_scroll_bar().step = 0.01
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+	
+func _physics_process(delta):
+	vertical_auto_scroll_with_rescroll()
 	pass
 
 
@@ -19,3 +25,14 @@ func set_prompt_text(_text: String):
 
 func attach_answer_card(_node): 
 	answers_box.add_child(_node)
+
+
+func vertical_auto_scroll_with_rescroll():
+	var scroll_bar = prompt_text_label.get_v_scroll_bar()
+	var current_scroll_value: float = scroll_bar.value
+	if scroll_bar.max_value > 25: ##this value is dependant on the size of the box and will need to be tweaked
+		if current_scroll_value >= scroll_bar.max_value:
+			scroll_bar.value = scroll_bar.min_value
+		else:
+			current_scroll_value += 0.35
+			scroll_bar.value = current_scroll_value
--- bubbly!/Scenes/PartyScreen/response_box.gd
+++ bubbly!/Scenes/PartyScreen/response_box.gd
@@ -26,9 +26,10 @@ func set_answer_text(_answer_text):
 func vertical_auto_scroll_with_rescroll():
 	var scroll_bar = answer_label.get_v_scroll_bar()
 	var current_scroll_value: float = scroll_bar.value
-	if current_scroll_value >= scroll_bar.max_value:
-		scroll_bar.value = scroll_bar.min_value
-	else:
-		current_scroll_value += 0.35
-		scroll_bar.value = current_scroll_value
-	
+	if scroll_bar.max_value > 48: ##this value is dependant on the size of the box and will need to be tweaked
+		if current_scroll_value >= scroll_bar.max_value:
+			scroll_bar.value = scroll_bar.min_value
+		else:
+			current_scroll_value += 0.35
+			scroll_bar.value = current_scroll_value
+		
